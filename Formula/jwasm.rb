42f9ff56f4220ec6f5941a8064417b6bf85b741d
Bradley Bell <149551+btb@users.noreply.github.com>
2021-08-16 22:14:16 +0000

    jwasm: add 2.13 bottle.

--- Formula/jwasm.rb
+++ Formula/jwasm.rb
@@ -5,6 +5,12 @@ class Jwasm < Formula
   sha256 "82bc14860ec1d0552daeffbd202f83f9bba6a2756056b5e21ef81fabdb8e83a4"
   license "Watcom-1.0"
 
+  bottle do
+    root_url "https://github.com/btb/homebrew-open-watcom/releases/download/jwasm-2.13"
+    sha256 cellar: :any_skip_relocation, catalina:     "4a6c2fb4c486798d3f12132cbe357584da195aff4bbe7c8c73da5a28b0cafd6d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "af8ea1056e7549bbceda330c421d4ab8902ce3444c3e43eaa83d70b5de3a7db8"
+  end
+
   depends_on "cmake" => :build
 
   def install

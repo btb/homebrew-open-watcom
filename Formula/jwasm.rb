9b1a0a58c673de3b662f2eba69a63077c604d66e
Bradley Bell <bradley.bell@gmail.com>
2021-08-17 02:21:31 -0700

    different fork, update to 2.14

--- Formula/jwasm.rb
+++ Formula/jwasm.rb
@@ -1,8 +1,8 @@
 class Jwasm < Formula
-  desc "Free Masm-compatible assembler"
-  homepage "https://jwasm.github.io"
-  url "https://github.com/JWasm/JWasm/archive/refs/tags/2.13.tar.gz"
-  sha256 "82bc14860ec1d0552daeffbd202f83f9bba6a2756056b5e21ef81fabdb8e83a4"
+  desc "Masm-compatible assembler"
+  homepage "https://github.com/Baron-von-Riedesel/JWasm"
+  url "https://github.com/Baron-von-Riedesel/JWasm/archive/refs/tags/v2.14.tar.gz"
+  sha256 "633d94bd8bb8f5915dd3338d69bc8cb48dbcd923e013c6ee1456cd6e13fc3a5a"
   license "Watcom-1.0"
 
   bottle do
@@ -11,15 +11,18 @@ class Jwasm < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux: "af8ea1056e7549bbceda330c421d4ab8902ce3444c3e43eaa83d70b5de3a7db8"
   end
 
-  depends_on "cmake" => :build
+  patch do
+    url "https://github.com/btb/JWasm/commit/0f666f8a8f4794b8e7ed1081ddcdc02b83313853.patch?full_index=1"
+    sha256 "3e3290d854121e63cc1adb067c30450d1cb917fea5157d732118b419971213ed"
+  end
 
   def install
-    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-    system "cmake", "--build", "build"
+    system "make", "-f", "GccUnix.mak"
 
-    bin.install "build/jwasm"
-    prefix.install "History.txt", "License.txt", "Readme.txt"
-    doc.install Dir["Doc/*"]
+    bin.install "build/GccUnixR/jwasm"
+    prefix.install "History.txt", "README.md"
+    doc.install "Html/License.html", "Html/Manual.html", "Html/Readme.html"
+    pkgshare.install "Samples"
   end
 
   test do
